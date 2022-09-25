f4d8cb12a9845a76c0d2c898ae5b337de352990b
Daiana de Paula <daianaadepaula1@gmail.com>
2022-09-25 08:11:41 -0300

    created front-end

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const express = require('express')
 const app = express()
+const hbs = require('express-handlebars')
 const PORT = process.env.PORT || 3001
 
+//CONFIGURAÇÃO DO HANDLEBARS
+app.engine('hbs', hbs.engine({
+	extname: 'hbs',
+	defaultLayout: 'main'
+})); app.set('view engine', 'hbs')
+
+//REFERENCIA CSS E JS
+app.use(express.static('public'))
+
 app.get('/', (req, res)=>{
-	res.send('Hello Word')
+	res.render('index', {NavActiveCard:true})
+})
+
+app.get('/users', (req, res)=>{
+	res.render('users', {NavActiveUsers:true})
+})
+
+app.get('/editar', (req, res)=>{
+	res.render('editar')
 })
 
 app.listen(PORT, ()=>{
